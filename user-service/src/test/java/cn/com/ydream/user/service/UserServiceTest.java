68c573ef20960f4411ef2547d824e95f6ff945e8
tianzhuchen1988 <36905602@qq.com>
2018-08-28 20:50:57 +0800

    修复mysql url

--- user-service/src/test/java/cn/com/ydream/user/service/UserServiceTest.java
+++ user-service/src/test/java/cn/com/ydream/user/service/UserServiceTest.java
@@ -1,27 +1,59 @@
 package cn.com.ydream.user.service;
 
+import cn.com.ydream.user.client.AuthFeignClient;
+import cn.com.ydream.user.client.ProductFeignClient;
 import cn.com.ydream.user.domain.User;
+import cn.com.ydream.user.repository.UserRepository;
+import cn.com.ydream.user.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class UserServiceTest {
 
-    @Autowired
-    private UserService userService;
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private ProductFeignClient productFeignClient;
+
+    @Mock
+    private AuthFeignClient authFeignClient;
+
+    @Mock
+    private Environment env;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
 
     @Test
     public void shouldFindById(){
-        User find = userService.findUserById(1003);
-        log.info(find.getUserName());
+        final User user = new User();
+        user.setUserId(1003);
+
+        User find = userService.findUserById(user.getUserId());
+
+        assertNull(find);
     }
 
 }
